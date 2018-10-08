ea46aced8c38e6b772d90b0fe2bbf7403cc570bb
Armastus <david.o.bustillos@gmail.com>
2018-10-07 18:53:40 -0700

    afdasfdfsadfd

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ let personRoute = require('./routes/person');
 let customerRoute = require('./routes/customer');
 let path = require('path');
 let bodyParser = require('body-parser');
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -33,4 +34,27 @@ app.use((err, req, res, next) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.info(`Server has started on ${PORT}`))
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
+
+function doesConnectionExist() {
+    var xhr = new XMLHttpRequest();
+    var file = "https://www.kirupa.com/blank.png";
+    var randomNum = Math.round(Math.random() * 10000);
+
+    xhr.open('HEAD', file + "?rand=" + randomNum, true);
+    xhr.send();
+
+    xhr.addEventListener("readystatechange", processRequest, false);
+
+    function processRequest(e) {
+        if (xhr.readyState == 4) {
+            if (xhr.status >= 200 && xhr.status < 304) {
+                console.log("connection exists!");
+            } else {
+                console.log("connection doesn't exist!");
+            }
+        }
+    }
+};
+
+doesConnectionExist();
